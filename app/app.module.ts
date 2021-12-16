13c34497e02bf6af53d196f0a88e9209eca2685b
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2021-12-16 15:15:08 -0300

    step 4 Refactor phone.service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UpgradeModule } from '@angular/upgrade/static';
+import { HttpClientModule } from '@angular/common/http';
+
+import { Phone } from './core/phone/phone.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     UpgradeModule,
+    HttpClientModule
   ],
+  providers: [
+    Phone,
+  ]
 })
 export class AppModule {
   constructor(private upgrade: UpgradeModule) { }
